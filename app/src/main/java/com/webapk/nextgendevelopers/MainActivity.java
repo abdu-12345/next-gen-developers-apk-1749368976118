993463d1ac36c270c2306a6e5dd13ae2382070cb
abdu-12345 <nextgendevelopers98@gmail.com>
2025-06-08 11:49:40 +0400

    Add app/src/main/java/com/webapk/nextgendevelopers/MainActivity.java

---
+++ app/src/main/java/com/webapk/nextgendevelopers/MainActivity.java
@@ -0,0 +1,43 @@
+
+package com.webapk.nextgendevelopers;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://nextgendevelopers-app.lovable.app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
